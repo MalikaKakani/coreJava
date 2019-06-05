38e0d55aa4d7bd9a71ffa24cf2a32576b4e8911d
unknown <kmalika321@gmail.com>
2019-06-04 23:12:30 -0400

    updated overLoading method

--- src/com/malika/datatypes/OverLoading.java
+++ src/com/malika/datatypes/OverLoading.java
@@ -17,11 +17,25 @@ public class OverLoading {
         System.out.println( " Display my Initial " +c );
     }
 
+    public int calculation(int a, int b)
+    {
+        System.out.println(" addition of 2 vars are " +(a+b));
+        return a+b;
+    }
+
+    public int calculation(int a, int b, int c)
+    {
+        System.out.println(" addition of 3 vars are " +(a+b+c));
+        return a+b+c;
+    }
+
     public static void main(String args[])
     {
         OverLoading overLoading = new OverLoading();
         overLoading.school(10);
         overLoading.school(12,14);
         overLoading.school('K');
+        overLoading.calculation(12, 13);
+        overLoading.calculation(12, 13, 25);
     }
 }
